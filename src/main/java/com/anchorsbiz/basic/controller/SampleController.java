60617c00c8f42fc6e45de96e93c32ab5e296efeb
모함메드 <103490565+mohamedashour89@users.noreply.github.com>
2025-02-17 17:58:28 +0900

    first task

--- src/main/java/com/anchorsbiz/basic/controller/SampleController.java
+++ src/main/java/com/anchorsbiz/basic/controller/SampleController.java
@@ -1,4 +1,22 @@
 package com.anchorsbiz.basic.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.anchorsbiz.basic.service.SampleService;
+@RestController
+@RequestMapping("/api/spring/sample")
 public class SampleController {
+
+    private final SampleService sampleService;
+
+    public SampleController(SampleService sampleService) {
+        this.sampleService = sampleService;
+    }
+
+    @GetMapping
+    public String printMessage() {
+        return sampleService.printMessage();  // Use the returned message from service
+    }
 }
--- src/main/java/com/anchorsbiz/basic/service/SampleService.java
+++ src/main/java/com/anchorsbiz/basic/service/SampleService.java
@@ -1,4 +1,13 @@
 package com.anchorsbiz.basic.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SampleService {
+    
+    public String printMessage() {
+        String message = "Hello World";
+        System.out.println(message);  
+        return message;
+    }
 }
